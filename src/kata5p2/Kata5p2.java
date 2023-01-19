d09bb1535dee89791440707e142b653ee9aff50f
yabpe <yabpe@10.193.45.80>
2023-01-19 16:30:23 +0000

    Tercer Commit v.2 (Corregido para usar build())

--- src/kata5p2/Kata5p2.java
+++ src/kata5p2/Kata5p2.java
@@ -2,22 +2,16 @@ package kata5p2;
 
 import kata5p2.view.HistogramDisplay;
 import kata5p2.model.Mail;
-import java.io.File;
 import java.util.List;
 import kata5p2.model.Histogram;
+import static kata5p2.view.MailHistogramBuilder.build;
 import static kata5p2.view.MailListReaderBD.read;
 
 public class Kata5p2 {
 
     public static void main(String[] args) {
-        Histogram<String> histogram = new Histogram<String>();
         List<Mail> emails = read();
-        for (Mail email : emails) {
-            //Se podria constuir para contar los dominios de mail con getDomain() o con los mails con getMail()
-            histogram.increment(email.getDomain());
-            
-        }
-
+        Histogram<String> histogram = build(emails);
         HistogramDisplay histogramDisplay = new HistogramDisplay("Histograma",
                 histogram);
         histogramDisplay.execute();
--- src/kata5p2/view/MailHistogramBuilder.java
+++ src/kata5p2/view/MailHistogramBuilder.java
@@ -4,16 +4,11 @@ import kata5p2.model.Mail;
 import kata5p2.model.Histogram;
 import java.util.List;
 public class MailHistogramBuilder {
-    private String fileName;
-    public MailHistogramBuilder(String filename){
-        fileName = filename;
-    }
-    public Histogram<String> build(List<Mail> mails){
-        Histogram<String> histogram = new Histogram<String>();
-        for (Mail mail : mails) {
+    public static Histogram<String> build(List<Mail> mails){
+        Histogram<String> histogram = new Histogram<>();
+        mails.forEach(mail -> {
             histogram.increment(mail.getDomain());
-            
-        }
+        });
         return histogram;
         
     }
